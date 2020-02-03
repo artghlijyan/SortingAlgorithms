bde04c947eed1aabba25977381bef5b271a45807
Art <deckuser@hotmail.com>
2020-02-03 17:32:42 +0400

    Add project files.

---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 9, 6, 0, 4 };
+
+            //arr.BubbleSort();
+            //arr.InsertionSort();
+            //arr.SelectionSort();
+            //arr.MergeSort();
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Swap(this int[] items, int left, int right)
+        {
+            if (left < 0 && right < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (left != right)
+            {
+                int temp = items[left];
+                items[left] = items[right];
+                items[right] = temp;
+            }
+        }
+
+        static void BubbleSort(this int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 1; j < arr.Length - i; j++)
+                {
+                    if (arr[j - 1].CompareTo(arr[j]) > 0)
+                    {
+                        arr.Swap(j - 1, j);
+                    }
+                }
+            }
+        }
+
+        static void InsertionSort(this int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (arr[j].CompareTo(arr[j - 1]) < 0)
+                    {
+                        arr.Swap(j - 1, j);
+                    }
+                }
+            }
+        }
+
+        static void SelectionSort(this int[] arr)
+        {
+            int min;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                min = arr[i];
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j].CompareTo(min) > 0)
+                    {
+                        min = arr[j];
+                        arr.Swap(i, j);
+                    }
+                }
+            }
+        }
+
+        static void MergeSort(this int[] arr)
+        {
+            if (arr.Length <= 1)
+            {
+                return;
+            }
+
+            int leftSize = arr.Length / 2;
+            int rightSize = arr.Length - leftSize;
+
+            int[] leftArr = new int[leftSize];
+            int[] rightArr = new int[rightSize];
+
+            Array.Copy(arr, 0, leftArr, 0, leftSize);
+            Array.Copy(arr, leftSize, rightArr, 0, rightSize);
+
+            MergeSort(leftArr);
+            MergeSort(rightArr);
+
+            Merge(arr, leftArr, rightArr);
+        }
+
+        private static void Merge(int[] array, int[] leftArr, int[] rightArr)
+        {
+            int leftIndex = 0;
+            int rightIndex = 0;
+            int targetIndex = 0;
+            int remaining = leftArr.Length + rightArr.Length;
+
+            while (remaining > 0)
+            {
+                if (leftIndex >= leftArr.Length)
+                {
+                    array[targetIndex] = rightArr[rightIndex++];
+                }
+                else if (rightIndex >= rightArr.Length)
+                {
+                    array[targetIndex] = leftArr[leftIndex++];
+                }
+                else if (leftArr[leftIndex].CompareTo(rightArr[rightIndex]) < 0)
+                {
+                    array[targetIndex] = leftArr[leftIndex++];
+                }
+                else
+                {
+                    array[targetIndex] = rightArr[rightIndex++];
+                }
+
+                targetIndex++;
+                remaining--;
+            }
+        }
+    }
+}
